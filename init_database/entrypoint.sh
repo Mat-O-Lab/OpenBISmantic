02037c04f7b0b149d6559076f64f5d34707fb72d
Thomas Hanke <hanke@docker-dev.iwm.fraunhofer.de>
2023-02-08 10:56:12 +0100

    added git clone to entrypoint.sh, deleted submodule

--- init_database/entrypoint.sh
+++ init_database/entrypoint.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 echo "started init_database"
+
+echo "import lebeDigital data"
+git clone https://github.com/BAMresearch/LebeDigital.git lebeDigital
+cd lebeDigital
+
+echo "building conda environment"
+# build enviroment
+conda env create -f environment.yml
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate lebedigital
+
+echo "uploading data"
+# upload data
+cd usecases/MinimumWorkingExample
+## doit runson=notactions url=https://openbis.matolab.org/openbis/ space=EMODUL user=admin force=yes mode=full # with password prompt
+doit runson=notactions url=https://$HOST_NAME:$OPENBIS_PORT/openbis/ space=EMODUL user=admin pw=${ADMIN_PASS} force=yes mode=full
