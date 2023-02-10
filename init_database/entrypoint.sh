ef6c60fef0e54ed56f7ca78e1184bc73c940d8e5
Thomas Hanke <hanke@docker-dev.iwm.fraunhofer.de>
2023-02-10 09:29:54 +0100

    added a check for login to init container

--- init_database/entrypoint.sh
+++ init_database/entrypoint.sh
@@ -5,6 +5,25 @@ echo "started init_database"
 echo "monkey patching to disable certificate verification"
 sed -i 's/verify_certificates=True,/verify_certificates=False,/g' /examples/LebeDigital/lebedigital/openbis/interbis.py
 
+echo "check if login works"
+python << END
+import os
+from pybis import Openbis
+# Get environment variables
+SERVER_HOST_PORT = os.getenv('SERVER_HOST_PORT')
+ADMIN_PASS = os.environ.get('ADMIN_PASS')
+o = Openbis(SERVER_HOST_PORT,verify_certificates=False)
+print(o.login('admin', ADMIN_PASS, save_token=True))
+END
+if [ $? -eq 0 ]
+then
+  echo "Successfully login."
+else
+  # Redirect stdout from echo command to stderr.
+  echo "Login not working. Sleeping 30s then exit"
+  sleep 30s
+  exit 13;
+fi
 
 echo "uploading data"
 # upload data
@@ -12,3 +31,4 @@ cd /examples/LebeDigital/usecases/MinimumWorkingExample
 echo "https://$SERVER_HOST_PORT/openbis/"
 ## doit runson=notactions url=https://openbis.matolab.org/openbis/ space=EMODUL user=admin force=yes mode=full # with password prompt
 doit runson=notactions url=https://$SERVER_HOST_PORT/openbis/ space=EMODUL user=admin pw=${ADMIN_PASS} force=yes mode=full
+exit 0
