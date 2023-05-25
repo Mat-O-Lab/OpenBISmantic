67ae88a0e67318734b47b191d452d84ba03ceecc
Rasmus Antons <rasmus.antons@iwm.fraunhofer.de>
2023-05-25 11:25:08 +0200

    remove openbismantic-plugin, openbismantic_api changes

--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,6 @@ WORKDIR /home/openbis/openbis/servers
 RUN cp ./openBIS-server/jetty-dist/demo-base/start.d/http.ini ./openBIS-server/jetty/start.d/
 COPY ./datastore_service/service.properties ./datastore_server/etc/service.properties
 COPY ./openbis-server/service.properties ./openBIS-server/jetty/etc/service.properties
-#install openbismatic plugin
-# COPY ./OpenBISmantic-plugin ./core-plugins/openbismantic
-ADD ./OpenBISmantic-plugin ./core-plugins/openbismantic
 RUN echo 'enabled-modules = dropbox-monitor, dataset-uploader, dataset-file-search, xls-import, openbis-sync, eln-lims, openbis-ng-ui, search-store, openbismantic' > ./core-plugins/core-plugins.properties
 #add code to set admin password
 RUN sed -i '2s/^/#set admin password\necho "setting admin password"\n\/home\/openbis\/openbis\/servers\/openBIS-server\/jetty\/bin\/passwd.sh change -p ${ADMIN_PASS} admin\n /' /usr/local/bin/docker-entrypoint.sh
--- openbismantic_api/app.py
+++ openbismantic_api/app.py
@@ -2,7 +2,8 @@ import base64
 import io
 
 from fastapi import FastAPI, Request, Response
-from starlette.responses import RedirectResponse, FileResponse
+from fastapi.templating import Jinja2Templates
+from starlette.responses import RedirectResponse
 import pybis
 from openbis_json_parser import parse_dict
 from openbis_json_parser.parser import write_ontology
@@ -10,15 +11,34 @@ from openbis_json_parser.parser import write_ontology
 app = FastAPI(
     title='OpenBISmantic API',
 )
+templates = Jinja2Templates(directory='templates')
 
 
-class TurtleResponse(Response):
-    media_type = 'application/turtle'
+class OpenBISmanticResponse(Response):
+    media_type = 'application/x-turtle'
+
+    def __init__(self, *args, request=None, **kwargs):
+        self.request = request
+        accept_header = request.headers.get('accept', '').split(',')
+        if 'text/html' in accept_header:
+            self.media_type = 'text/html'
+        elif 'application/turtle' in accept_header:
+            self.media_type = 'application/x-turtle'
+        elif 'application/ld+json' in accept_header:
+            self.media_type = 'application/ld+json'
+        super().__init__(*args, **kwargs)
 
     def render(self, content):
         onto = parse_dict(content)
+        if self.media_type == 'text/html':
+            template = templates.get_template('app.html')
+            return template.render({'request': self.request, 'onto': onto})
         out_file = io.BytesIO()
-        write_ontology(onto, out_file, 'turtle')
+        export_format = {
+            'application/x-turtle': 'turtle',
+            'application/ld+json': 'json-ld'
+        }.get(self.media_type, 'turtle')
+        write_ontology(onto, out_file, export_format)
         out_file.seek(0)
         return out_file.read()
 
@@ -32,31 +52,22 @@ async def add_pybis_session(request: Request, call_next):
         auth_type, auth_value = auth_header.split(' ', 1)
         if auth_type.lower() == 'basic':
             un, pw = base64.b64decode(auth_value).decode('utf-8').split(':')
-            print(f'logging in using {un=} {pw=}')
-            bis.login(un, pw)
+            bis.login(un, pw)  # todo: catch exception?
         elif auth_value.lower() == 'token':
-            print(f'logging in using token={auth_value}')
-            bis.set_token(auth_value)
+            bis.__dict__['token'] = auth_value
     elif 'text/html' in accept_header:
         session_token = request.cookies.get('openbis')
         if session_token is None or session_token == 'null':
             return RedirectResponse(url='/openbis/webapp/eln-lims/')
-        print(f'logging in using token={session_token}')
-        bis.set_token(session_token)
+        bis.__dict__['token'] = session_token
     if not bis.is_session_active():
-        return Response(status_code=401)
+        if 'text/html' in accept_header:
+            return RedirectResponse(url='/openbis/webapp/eln-lims/')
+        else:
+            return Response(status_code=401)
     request.state.bis = bis
     response = await call_next(request)
     return response
-
-
-@app.middleware('http')
-async def openbismantic_middleware(request: Request, call_next):
-    accept_header = request.headers.get('accept', '').split(',')
-    if 'text/html' in accept_header:
-        return FileResponse('app.html')
-    else:
-        return await call_next(request)
 
 
 @app.get('/')
@@ -67,5 +78,8 @@ async def index():
 @app.get('/sample/{perm_id}')
 async def get_sample(perm_id: str, request: Request):
     bis: pybis.Openbis = request.state.bis
-    sample = bis.get_sample(perm_id)
-    return TurtleResponse(sample.data)
+    try:
+        sample = bis.get_sample(perm_id)
+    except ValueError:
+        return Response('sample not found', status_code=404)
+    return OpenBISmanticResponse(sample.data, request=request)
