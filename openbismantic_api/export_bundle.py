7ed49174414046753ed833e419698b823bd54948
Rasmus Antons <rasmus.antons@iwm.fraunhofer.de>
2024-09-12 14:12:52 +0200

    add prov metadata to export bundle

--- openbismantic_api/export_bundle.py
+++ openbismantic_api/export_bundle.py
@@ -1,3 +1,4 @@
+import datetime
 import io
 import sys
 import zipfile
@@ -17,8 +18,9 @@ PROV = rdflib.Namespace('http://www.w3.org/ns/prov#')
 
 def export_from_graph(bis: pybis.Openbis, graph: rdflib.Graph):
     distribution_query = """
-    SELECT ?dist ?dataset ?perm_id ?dl_url WHERE {
-        ?dist a <http://www.w3.org/ns/dcat#Distribution>.
+    SELECT ?dist ?byte_size ?dataset ?perm_id ?dl_url WHERE {
+        ?dist a <http://www.w3.org/ns/dcat#Distribution>;
+              <http://www.w3.org/ns/dcat#byteSize> ?byte_size.
         ?dataset <http://www.w3.org/ns/dcat#distribution> ?dist;
                  <http://w3id.org/matolab/openbis/code> ?perm_id;
                  <http://w3id.org/matolab/openbis/relates_to> ?datastore.
@@ -29,13 +31,24 @@ def export_from_graph(bis: pybis.Openbis, graph: rdflib.Graph):
 
     zb = io.BytesIO()
     with zipfile.ZipFile(zb, 'a', zipfile.ZIP_DEFLATED, False) as zf:
-        graph.add((rdflib.URIRef('./'), RDF_SYNTAX.type, SCHEMA.Dataset))
+        ro_root = rdflib.URIRef('./')
+        graph.add((ro_root, RDF_SYNTAX.type, SCHEMA.Dataset))
+        prov_activity = rdflib.BNode()
+        graph.add((prov_activity, RDF_SYNTAX.type, PROV.Activity))
+        graph.add((prov_activity, PROV.startedAtTime, rdflib.Literal(datetime.datetime.now())))
         for query_result in graph.query(distribution_query):
-            dist, dataset, perm_id, dl_url = query_result
+            dist, byte_size, dataset, perm_id, dl_url = query_result
             dist_path = str(dist)[str(dist).find(perm_id.value):]
-            file_node = rdflib.URIRef(dist_path)
-            graph.add((file_node, RDF_SYNTAX.type, SCHEMA.File))
-            graph.add((file_node, PROV.wasDerivedFrom, dist))
+            ro_dataset = rdflib.URIRef(perm_id)
+            graph.add((ro_dataset, RDF_SYNTAX.type, SCHEMA.Dataset))
+            graph.add((ro_root, SCHEMA.hasPart, ro_dataset))
+            graph.add((ro_dataset, PROV.wasDerivedFrom, dataset))
+            ro_file = rdflib.URIRef(dist_path)
+            graph.add((prov_activity, PROV.used, dist))
+            graph.add((ro_file, RDF_SYNTAX.type, SCHEMA.File))
+            graph.add((ro_dataset, SCHEMA.hasPart, SCHEMA.File))
+            graph.add((ro_file, PROV.wasDerivedFrom, dist))
+            graph.add((ro_file, SCHEMA.contentSize, byte_size))
             dl_url = f'{dl_url.value}/datastore_server/{dist_path}'
             base_url = os.environ.get('BASE_URL', None)
             verify = not bool(os.environ.get('NO_VERIFY_CERTIFICATES', False))
@@ -47,7 +60,7 @@ def export_from_graph(bis: pybis.Openbis, graph: rdflib.Graph):
                     print(f'failed to download file {dl_url}: {dl_res.status_code}', sys.stderr)
             else:
                 print(f'not allowed to download {dl_url}', sys.stderr)
-
+        graph.add((prov_activity, PROV.endedAtTime, rdflib.Literal(datetime.datetime.now())))
         gb = io.BytesIO()
         graph.serialize(gb, 'json-ld')
         zf.writestr('ro-crate-metadata.json', gb.getvalue())
